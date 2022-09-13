ac6ba4bc094a6722f339af93354cabb939349341
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-09-13 08:01:52 +0200

    Add files via upload

---
+++ Java/Java Output Formatting.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i = 0; i < 3 ; i++)
+        { 
+            String s = input.next();
+            int n = input.nextInt();
+             
+            System.out.printf("%-15s%03d\n",  s, n);
+            
+        }
+        System.out.print("================================");
+    }
+}
