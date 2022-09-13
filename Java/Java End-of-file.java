e55e2b6090f3bac1520a80b4e5c250bc5c105d1f
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-09-13 08:13:29 +0200

    Add files via upload

---
+++ Java/Java End-of-file.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int cnt = 1;
+        
+        while(input.hasNext())
+            System.out.println(cnt++ + " " + input.nextLine());
+    }
+}
