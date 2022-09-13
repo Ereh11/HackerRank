b478b18f93fa11f3f78b795a002017c78baf7d10
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-09-13 07:52:57 +0200

    Add files via upload

---
+++ Java/Java Stdin and Stdout I.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int a, b, c; 
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+    }
+}
