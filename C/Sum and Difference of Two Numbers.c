443d42926c61175d500bd07f97f0209cb2da8970
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-10-05 18:50:07 +0200

    Add files via upload

---
+++ C/Sum and Difference of Two Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+    int a, b; float fa, fb;
+    
+    scanf("%d%d%f%f", &a, &b, &fa, &fb);
+    printf("%d %d\n%.1f %.1f", a + b, a - b, fa + fb, fa - fb);
+    
+    return 0;
+}
