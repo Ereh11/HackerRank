d6f216587db238e718028d3880039d1d1d5fc8bb
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-11-01 08:01:11 +0200

    Delete The PADS.sql

--- SQL/The PADS.sql
+++
@@ -1,9 +0,0 @@
-SELECT CONCAT(Name, '(', LEFT(occupation, 1), ')')
-FROM OCCUPATIONS
-ORDER BY Name;
-
-SELECT 'There are a total of', COUNT(*) AS CO, 
-       CONCAT(LOWER(LEFT(Occupation, 1)), SUBSTRING(Occupation, 2), 's', '.')
-FROM OCCUPATIONS
-GROUP BY Occupation
-ORDER BY CO;
