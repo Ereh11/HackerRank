351f2383dedc089f9d990881fe4a4ed34ca38397
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-11-01 07:35:49 +0200

    Delete The Report.sql

--- SQL/The Report.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN G.Grade >= 8 THEN S.Name ELSE NULL END, G.Grade, S.Marks
-
-FROM Students S, Grades G
-
-WHERE S.Marks BETWEEN Min_Mark AND Max_Mark
-
-ORDER BY G.Grade DESC, S.Name;
